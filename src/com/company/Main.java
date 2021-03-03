2e9cfaf0ce0d10f45fa565b1d9c69f70d2f76209
Jakub Wojtyniak <wojtyniak.jakub@wp.pl>
2021-03-03 19:49:19 +0100

    Zmiana wartości zwracanej przez metode Policz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
 public class Main {
-
+    private static int Policz(int a,int b){
+        return (a+b)*(a-b);
+    }
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println("W pierwszym branchu; 2+3="+Policz(2,3));
     }
 }
